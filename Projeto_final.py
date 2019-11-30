e092b330aec710cc5678fdaad0cebd1ed4ae9975
pedr0o7 <58218137+pedr0o7@users.noreply.github.com>
2019-11-30 00:27:21 -0300

    Update Projeto_final.py

--- Projeto_final.py
+++ Projeto_final.py
@@ -1,7 +1,6 @@
 import threading as _
 
-
-class Conta(object):
+class banco(object):
 
     def __init__(self, nome=None, saldo=0.00):
 
@@ -16,44 +15,46 @@ class Conta(object):
 
         return self.saldo
 
-    def transfere(self, conta, protect):
+    def transfere(self, conta, dudu):
+
         transferencia = 10
-        protect.acquire()
+        dudu.acquire()
+
         try:
+
             if self.saldo >= transferencia:
 
                 conta.saldo += transferencia
                 self.saldo -= transferencia
 
-
-                print(self.n,"{:.2f}".format( self.get_saldo()), "\n")
-                print(conta.get_nome(),"{:.2f}".format( conta.get_saldo()), "\n")
-                print('Transferencia efetuada\n')
+                print("\tsaldos atuais:\n")
+                print(self.n,"= {:.2f}".format( self.get_saldo()), "\n")
+                print(conta.get_nome(),"= {:.2f}".format( conta.get_saldo()), "\n")
+                print("\tTransferencia efetuada.")
                 print("-"*100)
 
         finally:
-            protect.release()
 
+            dudu.release()
 
 
 if __name__ == '__main__':
 
-    cliente1 = Conta('pedro', saldo=1000.00)
-    cliente2 = Conta('luana', saldo=0.00)
-    print("Saldo inicial:\n")
-    print(cliente1.get_nome(), cliente1.get_saldo(), "\n")
-    print(cliente2.get_nome(), cliente2.get_saldo(), "\n")
-    print('Transferencia efetuada\n')
-    print("-"*100,"\n")
+    cliente1 = banco("pedro", saldo=1000.00)
+    cliente2 = banco("luana", saldo=0.00)
 
+    print("\n\tSaldos iniciais:\n")
+    print(cliente1.get_nome(),"=",cliente1.get_saldo(), "\n")
+    print(cliente2.get_nome(),"=",cliente2.get_saldo(), "\n")
+    print("\tTransferencia efetuada.\n")
+    print("-"*100,"\n")
 
     lock = _.Lock()
 
-    aleatorio = 10
-
     for a in range(0, 100):
-        thread = _.Thread(target=cliente1.transfere, args=(cliente2, lock))
-        thread.start()
 
-        thread = _.Thread(target=cliente2.transfere, args=(cliente1, lock))
-        thread.start()
+        transacao = _.Thread(target=cliente1.transfere, args=(cliente2, lock))
+        transacao.start()
+
+        transacao2 = _.Thread(target=cliente2.transfere, args=(cliente1, lock))
+        transacao2.start()
